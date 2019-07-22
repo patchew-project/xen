3c6de76987ccbb17cd3ad15f0f5a7b50ce4a9d74
Roger Pau Monne <roger.pau@citrix.com>
2019-07-22 17:32:09 +0200

    x86/p2m: fix non-translated handling of iommu mappings
    The current usage of need_iommu_pt_sync in p2m for non-translated
    guests is wrong because it doesn't correctly handle a relaxed PV
    hardware domain, that has need_sync set to false, but still need
    entries to be added from calls to {set/clear}_identity_p2m_entry.
    
    Adjust the code in guest_physmap_add_page to also check whether the
    domain has an iommu instead of whether it needs syncing or not in
    order to take a reference to a page to be mapped.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Message-Id: <20190722153209.73107-1-roger.pau@citrix.com>
    Reviewed-by: Paul Durrant <paul.durrant@citrix.com>
    Tested-by: Roman Shaposhnik <roman@zededa.com>

--- xen/arch/x86/mm/p2m.c
+++ xen/arch/x86/mm/p2m.c
@@ -836,7 +836,7 @@ guest_physmap_add_page(struct domain *d, gfn_t gfn, mfn_t mfn,
          */
         for ( i = 0; i < (1UL << page_order); ++i, ++page )
         {
-            if ( !need_iommu_pt_sync(d) )
+            if ( !has_iommu_pt(d) )
                 /* nothing */;
             else if ( get_page_and_type(page, d, PGT_writable_page) )
                 put_page_and_type(page);
@@ -1341,7 +1341,7 @@ int set_identity_p2m_entry(struct domain *d, unsigned long gfn_l,
 
     if ( !paging_mode_translate(p2m->domain) )
     {
-        if ( !need_iommu_pt_sync(d) )
+        if ( !has_iommu_pt(d) )
             return 0;
         return iommu_legacy_map(d, _dfn(gfn_l), _mfn(gfn_l), PAGE_ORDER_4K,
                                 IOMMUF_readable | IOMMUF_writable);
@@ -1432,7 +1432,7 @@ int clear_identity_p2m_entry(struct domain *d, unsigned long gfn_l)
 
     if ( !paging_mode_translate(d) )
     {
-        if ( !need_iommu_pt_sync(d) )
+        if ( !has_iommu_pt(d) )
             return 0;
         return iommu_legacy_unmap(d, _dfn(gfn_l), PAGE_ORDER_4K);
     }
