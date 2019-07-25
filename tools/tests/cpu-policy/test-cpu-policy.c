69b857c46363b56efdf9f4b1f9fc1e763cc4aa6d
Christopher Clark <christopher.w.clark@gmail.com>
2019-07-24 17:53:26 -0700

    tests/cpu-policy: fix format-overflow warning by null terminating strings
    gcc 9.1.0 reports:
    
    | test-cpu-policy.c:64:18: error: '%.12s' directive argument is not a nul-terminated string [-Werror=format-overflow=]
    |    64 |             fail("  Test '%.12s', expected vendor %u, got %u\n",
    |       |                  ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    | test-cpu-policy.c:20:12: note: in definition of macro 'fail'
    |    20 |     printf(fmt, ##__VA_ARGS__);                 \
    |       |            ^~~
    | test-cpu-policy.c:64:27: note: format string is defined here
    |    64 |             fail("  Test '%.12s', expected vendor %u, got %u\n",
    |       |                           ^~~~~
    | test-cpu-policy.c:44:7: note: referenced argument declared here
    |    44 |     } tests[] = {
    |       |       ^~~~~
    
    so increase the string array size by one character for the null string
    terminator and add another single char to the struct within the same
    union to balance it.
    
    Signed-off-by: Christopher Clark <christopher.clark6@baesystems.com>
    Message-Id: <20190725005326.3553-1-christopher.w.clark@gmail.com>

--- tools/tests/cpu-policy/test-cpu-policy.c
+++ tools/tests/cpu-policy/test-cpu-policy.c
@@ -35,9 +35,10 @@ static void test_vendor_identification(void)
 {
     static const struct test {
         union {
-            char ident[12];
+            char ident[13];
             struct {
                 uint32_t b, d, c;
+                char null_terminator;
             };
         };
         unsigned int vendor;
