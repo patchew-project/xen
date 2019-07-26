6230be4e3cc9cbce256d5d4d2363736c902a9a64
Andrew Cooper <andrew.cooper3@citrix.com>
2019-07-26 21:32:21 +0100

    xen/link: Introduce .bss.percpu.page_aligned
    Future changes are going to need to page align some percpu data.
    
    This means that the percpu area needs suitably aligning in the BSS so CPU0 has
    correctly aligned data.  Shuffle the exact link order of items within the BSS
    to give .bss.percpu.page_aligned appropriate alignment.
    
    In addition, we need to be able to specify an alignment attribute to
    __DEFINE_PER_CPU().  Rework it so the caller passes in all attributes, and
    adjust DEFINE_PER_CPU{,_READ_MOSTLY}() to match.  This has the added bonus
    that it is now possible to grep for .bss.percpu and find all the users.
    
    Finally, introduce DEFINE_PER_CPU_PAGE_ALIGNED() which uses both section and
    alignment attributes.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Message-Id: <20190726203222.4833-2-andrew.cooper3@citrix.com>

--- xen/include/xen/percpu.h
+++ xen/include/xen/percpu.h
@@ -9,9 +9,15 @@
  * The _##name concatenation is being used here to prevent 'name' from getting
  * macro expanded, while still allowing a per-architecture symbol name prefix.
  */
-#define DEFINE_PER_CPU(type, name) __DEFINE_PER_CPU(type, _##name, )
+#define DEFINE_PER_CPU(type, name) \
+    __DEFINE_PER_CPU(__section(".bss.percpu"), type, _ ## name)
+
+#define DEFINE_PER_CPU_PAGE_ALIGNED(type, name) \
+    __DEFINE_PER_CPU(__section(".bss.percpu.page_aligned") \
+                     __aligned(PAGE_SIZE), type, _ ## name)
+
 #define DEFINE_PER_CPU_READ_MOSTLY(type, name) \
-	__DEFINE_PER_CPU(type, _##name, .read_mostly)
+    __DEFINE_PER_CPU(__section(".bss.percpu.read_mostly"), type, _ ## name)
 
 #define get_per_cpu_var(var)  (per_cpu__##var)
 
