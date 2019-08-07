db85d4006c54c15d28a215c810b3f2494fc90fbf
Juergen Gross <jgross@suse.com>
2019-08-07 16:31:19 +0200

    xen: modify lock profiling interface
    Today adding locks located in a struct to lock profiling requires a
    unique type index for each structure. This makes it hard to add any
    new structure as the related sysctl interface needs to be changed, too.
    
    Instead of using an index the already existing struct name specified
    in lock_profile_register_struct() can be used as an identifier instead.
    
    Modify the sysctl interface to use the struct name instead of the type
    index and adapt the related coding accordingly. Instead of an array of
    struct anchors for lock profiling we now use a linked list for that
    purpose. Use the special idx value -1 for cases where the idx isn't
    relevant (global locks) and shouldn't be printed.
    
    Increment the sysctl.h interface version as an interface is being
    modified.
    
    Add the missing setting of profiling time to xc_lockprof_query().
    
    Add freeing element data when deregistering a structure.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Message-Id: <20190807143119.8360-5-jgross@suse.com>

--- tools/libxc/xc_misc.c
+++ tools/libxc/xc_misc.c
@@ -512,6 +512,7 @@ int xc_lockprof_query_number(xc_interface *xch,
     rc = do_sysctl(xch, &sysctl);
 
     *n_elems = sysctl.u.lockprof_op.nr_elem;
+    *time = sysctl.u.lockprof_op.time;
 
     return rc;
 }
