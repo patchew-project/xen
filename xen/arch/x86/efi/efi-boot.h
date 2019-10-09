cc6d34bda50bd48e2684fc6fe1dae1df3c84c8af
Igor Druzhinin <igor.druzhinin@citrix.com>
2019-10-09 21:40:02 +0100

    x86/efi: properly handle 0 in pixel reserved bitmask
    In some graphics modes firmware is allowed to return 0 in pixel reserved
    bitmask which doesn't go against UEFI Spec 2.8 (12.9 Graphics Output Protocol).
    
    Without this change non-TrueColor modes won't work which will cause
    GOP init to fail - observed while trying to boot EFI Xen with Cirrus VGA.
    
    Signed-off-by: Igor Druzhinin <igor.druzhinin@citrix.com>
    Message-Id: <1570653603-9889-3-git-send-email-igor.druzhinin@citrix.com>

--- xen/arch/x86/efi/efi-boot.h
+++ xen/arch/x86/efi/efi-boot.h
@@ -528,9 +528,15 @@ static void __init efi_arch_video_init(EFI_GRAPHICS_OUTPUT_PROTOCOL *gop,
         bpp = set_color(mode_info->PixelInformation.BlueMask, bpp,
                         &vga_console_info.u.vesa_lfb.blue_pos,
                         &vga_console_info.u.vesa_lfb.blue_size);
-        bpp = set_color(mode_info->PixelInformation.ReservedMask, bpp,
-                        &vga_console_info.u.vesa_lfb.rsvd_pos,
-                        &vga_console_info.u.vesa_lfb.rsvd_size);
+        if ( !mode_info->PixelInformation.ReservedMask )
+        {
+            vga_console_info.u.vesa_lfb.rsvd_pos = 0;
+            vga_console_info.u.vesa_lfb.rsvd_size = 0;
+        }
+        else
+            bpp = set_color(mode_info->PixelInformation.ReservedMask, bpp,
+                            &vga_console_info.u.vesa_lfb.rsvd_pos,
+                            &vga_console_info.u.vesa_lfb.rsvd_size);
         if ( bpp > 0 )
             break;
         /* fall through */
