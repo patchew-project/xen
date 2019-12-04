17ed5722a4b748328600f4e45f6ce618dfb57492
Roger Pau Monne <roger.pau@citrix.com>
2019-12-04 17:20:23 +0100

    x86/apic: force phys mode if interrupt remapping is disabled
    Cluster mode can only be used with interrupt remapping support, since
    the top 16bits of the APIC ID are filled with the cluster ID, and
    hence on systems where the physical ID is still smaller than 255 the
    cluster ID is not. Force x2APIC to use physical mode if there's no
    interrupt remapping support.
    
    Note that this requires a further patch in order to enable x2APIC
    without interrupt remapping support.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Message-Id: <20191204162025.37510-3-roger.pau@citrix.com>

--- xen/arch/x86/genapic/x2apic.c
+++ xen/arch/x86/genapic/x2apic.c
@@ -226,7 +226,23 @@ boolean_param("x2apic_phys", x2apic_phys);
 const struct genapic *__init apic_x2apic_probe(void)
 {
     if ( x2apic_phys < 0 )
-        x2apic_phys = !!(acpi_gbl_FADT.flags & ACPI_FADT_APIC_PHYSICAL);
+    {
+        if ( !iommu_intremap )
+            /*
+             * Force physical mode if there's no interrupt remapping support:
+             * the ID in clustered mode requires a 32 bit destination field due
+             * to the usage of the high 16 bits to store the cluster ID.
+             */
+            x2apic_phys = true;
+        else
+            x2apic_phys = !!(acpi_gbl_FADT.flags & ACPI_FADT_APIC_PHYSICAL);
+    }
+    else if ( !x2apic_phys && !iommu_intremap )
+    {
+        printk("WARNING: x2APIC cluster mode is not supported without interrupt remapping\n"
+               "x2APIC: forcing phys mode\n");
+        x2apic_phys = true;
+    }
 
     if ( x2apic_phys )
         return &apic_x2apic_phys;
