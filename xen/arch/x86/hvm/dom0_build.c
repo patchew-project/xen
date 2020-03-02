b92ab76ffc207191b5179b85c7530084dcf91942
Roger Pau Monne <roger.pau@citrix.com>
2020-03-02 16:55:09 +0100

    x86/dom0: improve PVH initrd and metadata placement
    Don't assume there's going to be enough space at the tail of the
    loaded kernel and instead try to find a suitable memory area where the
    initrd and metadata can be loaded.
    
    Reported-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Message-Id: <20200302155509.44753-1-roger.pau@citrix.com>

--- xen/arch/x86/hvm/dom0_build.c
+++ xen/arch/x86/hvm/dom0_build.c
@@ -491,6 +491,44 @@ static int __init pvh_populate_p2m(struct domain *d)
 #undef MB1_PAGES
 }
 
+static paddr_t find_memory(const struct domain *d, const struct elf_binary *elf,
+                           size_t size)
+{
+    paddr_t kernel_start = (paddr_t)elf->dest_base;
+    paddr_t kernel_end = (paddr_t)(elf->dest_base + elf->dest_size);
+    unsigned int i;
+
+    for ( i = 0; i < d->arch.nr_e820; i++ )
+    {
+        paddr_t start, end;
+
+        if ( d->arch.e820[i].addr < MB(1) &&
+             d->arch.e820[i].addr + d->arch.e820[i].size < MB(1) )
+            continue;
+
+        start = MAX(ROUNDUP(d->arch.e820[i].addr, PAGE_SIZE), MB(1));
+        end = d->arch.e820[i].addr + d->arch.e820[i].size;
+
+        /* Deal with the kernel being loaded in the region. */
+        if ( kernel_start <= start && kernel_end >= start )
+            /* Truncate the start of the region */
+            start = ROUNDUP(kernel_end, PAGE_SIZE);
+        else if ( kernel_start <= end && kernel_end >= end )
+            /* Truncate the end of the region */
+            end = kernel_start;
+        /* Pick the biggest of the split regions */
+        else if ( kernel_start - start > end - kernel_end )
+            end = kernel_start;
+        else
+            start = ROUNDUP(kernel_end, PAGE_SIZE);
+
+        if ( end - start >= size )
+            return start;
+    }
+
+    return INVALID_PADDR;
+}
+
 static int __init pvh_load_kernel(struct domain *d, const module_t *image,
                                   unsigned long image_headroom,
                                   module_t *initrd, void *image_base,
@@ -547,7 +585,18 @@ static int __init pvh_load_kernel(struct domain *d, const module_t *image,
         return rc;
     }
 
-    last_addr = ROUNDUP(parms.virt_kend - parms.virt_base, PAGE_SIZE);
+    last_addr = find_memory(d, &elf, sizeof(start_info) +
+                            initrd ? ROUNDUP(initrd->mod_end, PAGE_SIZE) +
+                                     sizeof(mod)
+                                   : 0 +
+                            cmdline ? ROUNDUP(strlen(cmdline) + 1,
+                                              elf_64bit(&elf) ? 8 : 4)
+                                    : 0);
+    if ( last_addr == INVALID_PADDR )
+    {
+        printk("Unable to find a memory region to load initrd and metadata\n");
+        return -ENOMEM;
+    }
 
     if ( initrd != NULL )
     {
