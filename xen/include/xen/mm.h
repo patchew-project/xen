f0f7f1ab79df626bf43796ae5771b0bde1431675
Roger Pau Monne <roger.pau@citrix.com>
2020-03-19 16:47:14 +0100

    x86/tlb: introduce a flush HVM ASIDs flag
    Introduce a specific flag to request a HVM guest linear TLB flush,
    which is an ASID/VPID tickle that forces a guest linear to guest
    physical TLB flush for all HVM guests.
    
    This was previously unconditionally done in each pre_flush call, but
    that's not required: HVM guests not using shadow don't require linear
    TLB flushes as Xen doesn't modify the guest page tables in that case
    (ie: when using HAP). Note that shadow paging code already takes care
    of issuing the necessary flushes when the shadow page tables are
    modified.
    
    In order to keep the previous behavior modify all shadow code TLB
    flushes to also flush the guest linear to physical TLB. I haven't
    looked at each specific shadow code TLB flush in order to figure out
    whether it actually requires a guest TLB flush or not, so there might
    be room for improvement in that regard.
    
    Also perform ASID/VPIT flushes when modifying the p2m tables as it's a
    requirement for AMD hardware. Finally keep the flush in
    switch_cr3_cr4, as it's not clear whether code could rely on
    switch_cr3_cr4 also performing a guest linear TLB flush. A following
    patch can remove the ASID/VPIT tickle from switch_cr3_cr4 if found to
    not be necessary.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Message-Id: <20200319154716.34556-2-roger.pau@citrix.com>

--- xen/include/xen/mm.h
+++ xen/include/xen/mm.h
@@ -644,7 +644,7 @@ static inline void filtered_flush_tlb_mask(uint32_t tlbflush_timestamp)
     if ( !cpumask_empty(&mask) )
     {
         perfc_incr(need_flush_tlb_flush);
-        flush_tlb_mask(&mask);
+        flush_mask(&mask, FLUSH_TLB | FLUSH_HVM_ASID_CORE);
     }
 }
 
