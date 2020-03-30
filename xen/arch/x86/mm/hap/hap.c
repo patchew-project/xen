5d255b6bda2904f0b6bae6d72fcef225342c81ed
Alexandru Isaila <aisaila@bitdefender.com>
2020-03-30 09:54:34 +0300

    x86/altp2m: Hypercall to set altp2m view visibility
    At this moment a guest can call vmfunc to change the altp2m view. This
    should be limited in order to avoid any unwanted view switch.
    
    The new xc_altp2m_set_visibility() solves this by making views invisible
    to vmfunc.
    This is done by having a separate arch.altp2m_working_eptp that is
    populated and made invalid in the same places as altp2m_eptp. This is
    written to EPTP_LIST_ADDR.
    The views are made in/visible by marking them with INVALID_MFN or
    copying them back from altp2m_eptp.
    To have consistency the visibility also applies to
    p2m_switch_domain_altp2m_by_id().
    
    The usage of this hypercall is aimed at dom0 having a logic with a number of views
    created and at some time there is a need to be sure that only some of the views
    can be switched, saving the rest and making them visible when the time
    is right.
    
    Note: If altp2m mode is set to mixed the guest is able to change the view
    visibility and then call vmfunc.
    
    Signed-off-by: Alexandru Isaila <aisaila@bitdefender.com>
    Message-Id: <20200330065434.5952-1-aisaila@bitdefender.com>

--- xen/arch/x86/mm/hap/hap.c
+++ xen/arch/x86/mm/hap/hap.c
@@ -493,8 +493,17 @@ int hap_enable(struct domain *d, u32 mode)
             goto out;
         }
 
+        if ( (d->arch.altp2m_working_eptp = alloc_xenheap_page()) == NULL )
+        {
+            rv = -ENOMEM;
+            goto out;
+        }
+
         for ( i = 0; i < MAX_EPTP; i++ )
+        {
             d->arch.altp2m_eptp[i] = mfn_x(INVALID_MFN);
+            d->arch.altp2m_working_eptp[i] = mfn_x(INVALID_MFN);
+        }
 
         for ( i = 0; i < MAX_ALTP2M; i++ )
         {
@@ -526,6 +535,12 @@ void hap_final_teardown(struct domain *d)
         {
             free_xenheap_page(d->arch.altp2m_eptp);
             d->arch.altp2m_eptp = NULL;
+        }
+
+        if ( d->arch.altp2m_working_eptp )
+        {
+            free_xenheap_page(d->arch.altp2m_working_eptp);
+            d->arch.altp2m_working_eptp = NULL;
         }
 
         for ( i = 0; i < MAX_ALTP2M; i++ )
