be1ab6a611b1892d4488c61b3a271a459272f016
Paul Durrant <pdurrant@amazon.com>
2020-10-08 19:57:35 +0100

    tools/libs/guest: add code to save a v4 libxc stream
    This patch adds the necessary code to save a REC_TYPE_DOMAIN_CONTEXT record,
    and stop saving the now obsolete REC_TYPE_SHARED_INFO and REC_TYPE_TSC_INFO
    records for PV guests.
    
    Signed-off-by: Paul Durrant <pdurrant@amazon.com>
    Message-Id: <20201008185735.29875-12-paul@xen.org>

--- tools/libs/guest/xg_sr_common_x86.h
+++ tools/libs/guest/xg_sr_common_x86.h
@@ -4,12 +4,6 @@
 #include "xg_sr_common.h"
 
 /*
- * Obtains a domains TSC information from Xen and writes a X86_TSC_INFO record
- * into the stream.
- */
-int write_x86_tsc_info(struct xc_sr_context *ctx);
-
-/*
  * Parses a X86_TSC_INFO record and applies the result to the domain.
  */
 int handle_x86_tsc_info(struct xc_sr_context *ctx, struct xc_sr_record *rec);
