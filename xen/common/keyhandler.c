34e25e406e63becaab8ced41507095e124e3c7e4
Juergen Gross <jgross@suse.com>
2020-11-20 14:13:06 +0100

    xen: add support for automatic debug key actions in case of crash
    When the host crashes it would sometimes be nice to have additional
    debug data available which could be produced via debug keys, but
    halting the server for manual intervention might be impossible due to
    the need to reboot/kexec rather sooner than later.
    
    Add support for automatic debug key actions in case of crashes which
    can be activated via boot- or runtime-parameter.
    
    Depending on the type of crash the desired data might be different, so
    support different settings for the possible types of crashes.
    
    The parameter is "crash-debug" with the following syntax:
    
      crash-debug-<type>=<string>
    
    with <type> being one of:
    
      panic, hwdom, watchdog, kexeccmd, debugkey
    
    and <string> a sequence of debug key characters with '+' having the
    special semantics of a 10 millisecond pause.
    
    So "crash-debug-watchdog=0+0qr" would result in special output in case
    of watchdog triggered crash (dom0 state, 10 ms pause, dom0 state,
    domain info, run queues).
    
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Message-Id: <20201120131306.24388-1-jgross@suse.com>

--- xen/common/keyhandler.c
+++ xen/common/keyhandler.c
@@ -3,7 +3,9 @@
  */
 
 #include <asm/regs.h>
+#include <xen/delay.h>
 #include <xen/keyhandler.h>
+#include <xen/param.h>
 #include <xen/shutdown.h>
 #include <xen/event.h>
 #include <xen/console.h>
@@ -504,6 +506,42 @@ void __init initialize_keytable(void)
         alt_key_handling = 1;
         printk(XENLOG_INFO "Defaulting to alternative key handling; "
                "send 'A' to switch to normal mode.\n");
+    }
+}
+
+#define CRASHACTION_SIZE  32
+static char crash_debug_panic[CRASHACTION_SIZE];
+static char crash_debug_hwdom[CRASHACTION_SIZE];
+static char crash_debug_watchdog[CRASHACTION_SIZE];
+static char crash_debug_kexeccmd[CRASHACTION_SIZE];
+static char crash_debug_debugkey[CRASHACTION_SIZE];
+
+static char *crash_action[CRASHREASON_N] = {
+    [CRASHREASON_PANIC] = crash_debug_panic,
+    [CRASHREASON_HWDOM] = crash_debug_hwdom,
+    [CRASHREASON_WATCHDOG] = crash_debug_watchdog,
+    [CRASHREASON_KEXECCMD] = crash_debug_kexeccmd,
+    [CRASHREASON_DEBUGKEY] = crash_debug_debugkey,
+};
+
+string_runtime_param("crash-debug-panic", crash_debug_panic);
+string_runtime_param("crash-debug-hwdom", crash_debug_hwdom);
+string_runtime_param("crash-debug-watchdog", crash_debug_watchdog);
+string_runtime_param("crash-debug-kexeccmd", crash_debug_kexeccmd);
+string_runtime_param("crash-debug-debugkey", crash_debug_debugkey);
+
+void keyhandler_crash_action(enum crash_reason reason)
+{
+    const char *action = crash_action[reason];
+    struct cpu_user_regs *regs = get_irq_regs() ? : guest_cpu_user_regs();
+
+    while ( *action )
+    {
+        if ( *action == '+' )
+            mdelay(10);
+        else
+            handle_keypress(*action, regs);
+        action++;
     }
 }
 
